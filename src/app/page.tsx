78197614574b8ead5931f16c63557d1ae421bf50
Ahmet Burak Arslan <arslanahmet4772@gmail.com>
2024-07-29 23:11:49 +0200

    I made the background black because my eye hurt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hello world
+    <main className="flex min-h-screen flex-col items-center justify-between p-24 bg-zinc-800">
+      <h1 className="text-white">Hello world</h1>
     </main>
   );
 }
