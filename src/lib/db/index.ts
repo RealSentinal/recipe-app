c3bfee4e34157c3ffcad73cddb5d155fe2ccf260
Burak <burak.yn.dev@gmail.com>
2024-07-31 20:29:16 +0300

    🗃️ Add example env file

--- src/lib/db/index.ts
+++ src/lib/db/index.ts
@@ -2,7 +2,7 @@ import {drizzle} from "drizzle-orm/node-postgres"
 import { Client } from "pg"
 
 const client = new Client({
-    connectionString: ""
+    connectionString: process.env["PG_CONNECTION_STRING"]
 })
 
 await client.connect();
